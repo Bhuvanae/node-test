1ce5d6ed6fee3def9a08d5153d5cc9d2a13add1d
Bhuvanae <bhuvaneshwaranrajasekran@gmail.com>
2024-04-25 09:43:57 +0530

    commit 2

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 
-
 app.get('/test',(req,res)=>{
     return res.send("express working success fully")
 })
 
+app.listen('3000',()=>{
+    console.log("connected successfully")
+})
